595238204b8f910a2d251ec11c344ba2ce3c5882
Tuan-Si Tran <tuan-si.tran@gmx.de>
2011-10-22 18:35:32 +0200

    added menu, better overview

--- src/archdbs/ArchDBS.java
+++ src/archdbs/ArchDBS.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package archdbs;
 
 import com.ibm.db2.jcc.am.InternalDate;
@@ -9,6 +5,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,53 +19,46 @@ public class ArchDBS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            // TODO code application logic here
-            Class.forName("com.ibm.db2.jcc.DB2Driver");
-            Connection connection = DriverManager.getConnection("jdbc:db2://localhost:50000/archdbs", "db2inst1", "hda");
-            
-            // Turn off autocommit
-            connection.setAutoCommit(false);
-            
-            long startTime = System.currentTimeMillis();
-            
-            PreparedStatement pStmt_Kunde = connection.prepareStatement("INSERT INTO Kunde (KNR, KNAME, KVORNAME) VALUES (?, ?, ?)");
-            PreparedStatement pStmt_Produkt = connection.prepareStatement("INSERT INTO Produkt (PID, PRODUKTNAME, PREIS) VALUES (?, ?, ?)");
-            PreparedStatement pStmt_Bestellung = connection.prepareStatement("INSERT INTO Bestellung (BID, KNR, PID, DATUM, STATUS) VALUES (?, ?, ?, ?, ?)");
-            
-            // Create all 'Kunden'
-            for(int i=1; i<=100;i++) {
-                pStmt_Kunde.setInt(1, i);
-                pStmt_Kunde.setString(2, "Name "+i);
-                pStmt_Kunde.setString(3, "Vorname "+i);
-                pStmt_Kunde.execute();
-            }
-            
-            // Create all 'Produkte'
-            for(int i=1;i<=1000;i++) {
-                pStmt_Produkt.setInt(1, i);
-                pStmt_Produkt.setString(2, "Produkt "+i);
-                pStmt_Produkt.setDouble(3, i);
-                pStmt_Produkt.execute();
-            }
-            
-            // Create all 'Bestellung'
-            for(int i=1;i<=750000;i++) {
-                pStmt_Bestellung.setInt(1, i);
-                pStmt_Bestellung.setInt(2, 1);//(i%101)+1);
-                pStmt_Bestellung.setInt(3, 1);//(i%1001)+1);
-                pStmt_Bestellung.setDate(4, new Date(2011,10,17));
-                pStmt_Bestellung.setInt(5, 1);//i%2);
-                pStmt_Bestellung.execute();
-            }
-            
-            connection.commit();
-            connection.close();
-            
-            System.out.println("Ergebnis: " + (System.currentTimeMillis() - startTime));
-            // Test 3
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	
+
+        System.out.println("==== Menue ====");
+        System.out.println("0 - Programm beenden");
+        System.out.println("==== P1 ====");
+        System.out.println("1 - createTestdata()");
+        System.out.println("==== P2 ====");
+        System.out.println("2 - Szenario 1");
+        System.out.println("3 - Szenario 2");
+        System.out.println("4 - Szenario 3");
+        System.out.println("5 - Szenario 4");
+        System.out.println("==== END ====");
+        System.out.print("Auswahl: ");
+        
+        Scanner scanner = new Scanner(System.in);
+	if (scanner.hasNextInt()) {
+		switch(scanner.nextInt()) {
+                    case 1:
+                        P1.createTestdata();
+                        break;
+                    case 2:
+                        P2.scenarioOne();
+                        break;
+                    case 3:
+                        P2.scenarioTwo();
+                        break;
+                    case 4:
+                        P2.scenarioThree();
+                        break;
+                    case 5:
+                        P2.scenarioFour();
+                        break;
+                    default:
+                        break;
+                }
+	} else {
+		System.out.println("Leider ungültige Eingabe.");
+	}
+
     }
+    
+    
 }
---
+++ src/archdbs/P2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archdbs;
+
+/**
+ *
+ * @author Tran & Kappes
+ */
+public class P2 {
+    public static void scenarioOne() {
+        // ToDo Code here
+    }
+    
+    public static void scenarioTwo() {
+        // ToDo Code here
+    }
+    
+    public static void scenarioThree() {
+        // ToDo Code here
+    }
+    
+    public static void scenarioFour() {
+        // ToDo Code here
+    }
+}
