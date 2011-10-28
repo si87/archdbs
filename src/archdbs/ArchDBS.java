51a976ff3361a4843c77f5062adf82fedac35fcc
Bene Dikt <benkap@web.de>
2011-10-28 12:49:38 +0200

    asdf

--- src/archdbs/ArchDBS.java
+++ src/archdbs/ArchDBS.java
@@ -25,7 +25,7 @@ public class ArchDBS {
         try {
             // TODO code application logic here
             Class.forName("com.ibm.db2.jcc.DB2Driver");
-            Connection connection = DriverManager.getConnection("jdbc:db2://localhost:50000/archdbs", "db2inst1", "hda");
+            Connection connection = DriverManager.getConnection("jdbc:db2://localhost:50000/archdbf", "db2inst1", "hda");
             
             // Turn off autocommit
             connection.setAutoCommit(false);
